504594ed5147ae27fd46499e576f555f19f0ff01
Irina <irisha1704@yandex.ru>
2022-06-04 22:34:23 +0200

    Added: parser for parameters from command line changed files_to_dictionary: storing relative path instead of full path created a draft for comparing files (dictionary elements)

--- main.py
+++ main.py
@@ -2,7 +2,26 @@ import sys
 import os.path
 import logging
 import hashlib
-from pathlib import Path
+import argparse
+
+
+class Repository():
+
+    def copy_file(self):
+        pass
+
+    def remove_file(self):
+        pass
+
+
+def create_parser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('-s', '--source')
+    parser.add_argument('-c', '--clone')
+    parser.add_argument('-i', '--interval')
+    parser.add_argument('-l', '--log')
+
+    return parser
 
 
 def input_validation(arg1, arg2, arg3, arg4):
@@ -29,17 +48,23 @@ def files_to_dictionary(directory):
     file_dictionary = {}
     for path, currentDirectory, files in os.walk(directory):
         for file in files:
-            file_hash = file_to_hash_sha256(os.path.join(path, file))
-            file_dictionary[os.path.join(path, file)] = file_hash
+            full_path = os.path.join(path, file)
+            file_hash = file_to_hash_sha256(full_path)
+            rel_path = os.path.relpath(full_path, start=directory)
+            file_dictionary[rel_path] = file_hash
     return file_dictionary
 
 
 if __name__ == "__main__":
-    input_validation(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-    original_directory = sys.argv[1]
-    clone_directory = sys.argv[2]
-    sync_interval = int(sys.argv[3])
-    log_file = sys.argv[4]
+    parser = create_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    input_validation(namespace.source,namespace.clone, namespace.interval, namespace.log)
+
+    original_directory = namespace.source
+    clone_directory = namespace.clone
+    sync_interval = int(namespace.interval)
+    log_file = namespace.log
 
     Log_Format = "%(levelname)s %(asctime)s - %(message)s"
 
@@ -55,5 +80,22 @@ if __name__ == "__main__":
     clone_files = files_to_dictionary(clone_directory)
     for key, value in origin_files.items():
         print(f"filename: {key}, hash: {value}")
+
+    for item1 in clone_files.items():
+        file_found = False
+        for item2 in origin_files.items():
+            if item1 == item2:
+                file_found = True
+        if not file_found:
+            remove_file(item1)
+
+    for item1 in origin_files.items():
+        file_found = False
+        for item2 in clone_files.items():
+            if item1 == item2:
+                file_found = True
+        if not file_found:
+            copy_file(item1)
+            
 
 
